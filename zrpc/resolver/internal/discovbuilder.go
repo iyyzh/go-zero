5e87a847ee3538149ad80fa1753036e12233024a
fang <fanglehang@icloud.com>
2024-09-04 12:21:46 +0800

    test

--- zrpc/resolver/internal/discovbuilder.go
+++ zrpc/resolver/internal/discovbuilder.go
@@ -9,10 +9,41 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-type discovBuilder struct{}
+type discovBuilder struct {
+	cc     resolver.ClientConn
+	update func()
+}
 
 func (b *discovBuilder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (
 	resolver.Resolver, error) {
+	b.cc = cc
+	if err := b.updateState(target); err != nil {
+		return nil, err
+	}
+
+	return &nopResolver{cc: cc}, nil
+}
+
+func (b *discovBuilder) Scheme() string {
+	return DiscovScheme
+}
+
+func (b *discovBuilder) updateState(target resolver.Target) error {
+	if b.update == nil {
+		update, err := b.buildEndpointsUpdater(target)
+		if err != nil {
+			return err
+		}
+
+		b.update = update
+	}
+
+	b.update()
+
+	return nil
+}
+
+func (b *discovBuilder) buildEndpointsUpdater(target resolver.Target) (func(), error) {
 	hosts := strings.FieldsFunc(targets.GetAuthority(target), func(r rune) bool {
 		return r == EndpointSepChar
 	})
@@ -22,24 +53,20 @@ func (b *discovBuilder) Build(target resolver.Target, cc resolver.ClientConn, _ 
 	}
 
 	update := func() {
-		var addrs []resolver.Address
-		for _, val := range subset(sub.Values(), subsetSize) {
+		vals := subset(sub.Values(), subsetSize)
+		addrs := make([]resolver.Address, 0, len(vals))
+		for _, val := range vals {
 			addrs = append(addrs, resolver.Address{
 				Addr: val,
 			})
 		}
-		if err := cc.UpdateState(resolver.State{
+		if err := b.cc.UpdateState(resolver.State{
 			Addresses: addrs,
 		}); err != nil {
 			logx.Error(err)
 		}
 	}
 	sub.AddListener(update)
-	update()
 
-	return &nopResolver{cc: cc}, nil
-}
-
-func (b *discovBuilder) Scheme() string {
-	return DiscovScheme
+	return update, nil
 }
